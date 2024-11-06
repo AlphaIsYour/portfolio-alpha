230cb977f32b5dee018a34f34f9488edd7ae9ca3
AlphaIsYour <alphrenoorz@gmail.com>
2024-11-06 17:32:08 +0700

    Memperbaiki tampilan form login

--- js/script2.js
+++ js/script2.js
@@ -1,31 +1,4 @@
-// // Array berisi kata-kata yang ingin ditampilkan
-// const words = [
-//   "Bagus",
-//   "Rahmat",
-//   "Farhan",
-//   "Budi",
-//   "Rian",
-//   "Dimas",
-//   "Fikri",
-//   "Nurul",
-//   "Ilham",
-//   "Alomani",
-//   "Amonali",
-//   "Anolami",
-//   "Anomali",
-//   "Wowo",
-//   "Owi",
-// ];
-
-// // Fungsi untuk memilih kata acak
-// function getRandomWord() {
-//   const randomIndex = Math.floor(Math.random() * words.length);
-//   return words[randomIndex];
-// }
-
-// Tampilkan kata acak di elemen dengan id "changing-word"
-document.getElementById("changing-word").textContent = getRandomWord();
-
+// program mengetik
 const words = [
   "Welcome to My Website",
   "Explore My Projects",
